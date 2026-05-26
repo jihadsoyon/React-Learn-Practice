e9763fad594157ea83c143a94b056c8ab78ee82f
jihadsoyon <jihadsoyon@gmail.com>
2026-05-26 10:41:16 +0600

    feat: add search functionality for filtering table rows

--- Problem-Solving/data-table-app/src/components/DataTable.jsx
+++ Problem-Solving/data-table-app/src/components/DataTable.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import users from "../data/users";
 
 const DataTable = () => {
+  const [search, setSearch] = useState("");
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="table-container">
+
+      <input
+        type="text"
+        placeholder="Search user..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="search-input"
+      />
+
       <table>
         <thead>
           <tr>
@@ -13,7 +29,7 @@ const DataTable = () => {
         </thead>
 
         <tbody>
-          {users.map((user) => (
+          {filteredUsers.map((user) => (
             <tr key={user.id}>
               <td>{user.name}</td>
               <td>{user.email}</td>
