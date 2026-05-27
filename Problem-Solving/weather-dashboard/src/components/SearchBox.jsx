6800519e84aca17b56dd9cf37815788a416cdff9
jihadsoyon <jihadsoyon@gmail.com>
2026-05-27 11:39:13 +0600

    feat: create reusable search box component

---
+++ Problem-Solving/weather-dashboard/src/components/SearchBox.jsx
@@ -0,0 +1,18 @@
+const SearchBox = ({ city, setCity, handleSearch }) => {
+  return (
+    <div className="search-box">
+      <input
+        type="text"
+        placeholder="Enter city..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <button onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchBox;
