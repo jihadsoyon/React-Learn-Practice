f332a3c3846ee9867b032dba3c8c605cab7a9d6f
jihadsoyon <jihadsoyon@gmail.com>
2026-05-27 12:15:30 +0600

    get api from openwethermap

--- Problem-Solving/weather-dashboard/src/api.js
+++ Problem-Solving/weather-dashboard/src/api.js
@@ -1,13 +1,15 @@
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "488549d2799fdbda5bbb7249043999cb";
 
 export const fetchWeather = async (city) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
 
-  if (!response.ok) {
-    throw new Error("City not found");
+  const data = await response.json();
+
+  if (data.cod !== 200) {
+    throw new Error(data.message);
   }
 
-  return response.json();
+  return data;
 };
