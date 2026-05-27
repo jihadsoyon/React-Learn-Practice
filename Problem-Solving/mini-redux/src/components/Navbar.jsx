23ef1d1f1cdfa859deac1d6c9337ec4866e085f9
jihadsoyon <jihadsoyon@gmail.com>
2026-05-28 05:31:17 +0600

    feat: create reusable navbar and theme toggle

---
+++ Problem-Solving/mini-redux/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import ThemeToggle from "./ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h2>Mini Redux</h2>
+      <ThemeToggle />
+    </nav>
+  );
+};
+
+export default Navbar;
