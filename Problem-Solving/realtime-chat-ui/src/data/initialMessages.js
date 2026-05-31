3fda93474673eb5d4bec919e76c7f0a0bd7d3fe0
jihadsoyon <jihadsoyon@gmail.com>
2026-05-31 12:04:35 +0600

    feat: create initial chat data structure

---
+++ Problem-Solving/realtime-chat-ui/src/data/initialMessages.js
@@ -0,0 +1,22 @@
+const initialMessages = [
+  {
+    id: 1,
+    sender: "Bot",
+    text: "Welcome to the chat!",
+    time: "10:00 AM",
+  },
+  {
+    id: 2,
+    sender: "You",
+    text: "Hello 👋",
+    time: "10:01 AM",
+  },
+  {
+    id: 3,
+    sender: "Bot",
+    text: "How can I help you today?",
+    time: "10:02 AM",
+  },
+];
+
+export default initialMessages;
