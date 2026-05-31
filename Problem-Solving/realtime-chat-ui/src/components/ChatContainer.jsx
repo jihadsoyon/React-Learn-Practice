3fecd1b053b028df775b0491771f977d567dd016
jihadsoyon <jihadsoyon@gmail.com>
2026-05-31 12:07:42 +0600

    feat: implement send message functionality

--- Problem-Solving/realtime-chat-ui/src/components/ChatContainer.jsx
+++ Problem-Solving/realtime-chat-ui/src/components/ChatContainer.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import initialMessages from "../data/initialMessages";
 import ChatHeader from "./ChatHeader";
 import MessageList from "./MessageList";
+import MessageInput from "./MessageInput";
 
 const ChatContainer = () => {
+  const [messages, setMessages] = useState(initialMessages);
+
+  const sendMessage = (text) => {
+    const newMessage = {
+      id: Date.now(),
+      sender: "You",
+      text,
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    };
+
+    setMessages((prev) => [...prev, newMessage]);
+  };
+
   return (
     <div className="chat-container">
       <ChatHeader />
-      <MessageList messages={initialMessages} />
+      <MessageList messages={messages} />
+      <MessageInput onSend={sendMessage} />
     </div>
   );
 };
