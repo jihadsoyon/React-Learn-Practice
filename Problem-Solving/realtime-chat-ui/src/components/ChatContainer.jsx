b005c5556b6bb81ddcc3a0740af8d2bfb90cd340
jihadsoyon <jihadsoyon@gmail.com>
2026-05-31 12:30:07 +0600

    feat: implement auto scroll and realtime bot response

--- Problem-Solving/realtime-chat-ui/src/components/ChatContainer.jsx
+++ Problem-Solving/realtime-chat-ui/src/components/ChatContainer.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import initialMessages from "../data/initialMessages";
 import ChatHeader from "./ChatHeader";
 import MessageList from "./MessageList";
 import MessageInput from "./MessageInput";
+import TypingIndicator from "./TypingIndicator";
+
+const botReplies = [
+  "Interesting! Tell me more.",
+  "I understand.",
+  "That sounds great 🚀",
+  "Can you explain further?",
+  "Thanks for sharing that.",
+];
 
 const ChatContainer = () => {
   const [messages, setMessages] = useState(initialMessages);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const bottomRef = useRef(null);
 
   const sendMessage = (text) => {
-    const newMessage = {
+    const userMessage = {
       id: Date.now(),
       sender: "You",
       text,
@@ -18,13 +30,45 @@ const ChatContainer = () => {
       }),
     };
 
-    setMessages((prev) => [...prev, newMessage]);
+    setMessages((prev) => [...prev, userMessage]);
+
+    setIsTyping(true);
+
+    setTimeout(() => {
+      const randomReply =
+        botReplies[Math.floor(Math.random() * botReplies.length)];
+
+      const botMessage = {
+        id: Date.now() + 1,
+        sender: "Bot",
+        text: randomReply,
+        time: new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+      };
+
+      setMessages((prev) => [...prev, botMessage]);
+      setIsTyping(false);
+    }, 1500);
   };
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [messages, isTyping]);
 
   return (
     <div className="chat-container">
       <ChatHeader />
+
       <MessageList messages={messages} />
+
+      {isTyping && <TypingIndicator />}
+
+      <div ref={bottomRef}></div>
+
       <MessageInput onSend={sendMessage} />
     </div>
   );
