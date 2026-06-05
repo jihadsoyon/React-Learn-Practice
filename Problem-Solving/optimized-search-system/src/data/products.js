9429c806af55917848c3ff5094263c2a53c019ae
jihadsoyon <jihadsoyon@gmail.com>
2026-06-05 20:51:01 +0600

    Add searchable dataset

---
+++ Problem-Solving/optimized-search-system/src/data/products.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: "React Developer"
+  },
+  {
+    id: 2,
+    name: "Next.js Developer"
+  },
+  {
+    id: 3,
+    name: "JavaScript Engineer"
+  },
+  {
+    id: 4,
+    name: "Frontend Architect"
+  },
+  {
+    id: 5,
+    name: "MERN Stack Developer"
+  },
+  {
+    id: 6,
+    name: "TypeScript Developer"
+  },
+  {
+    id: 7,
+    name: "Backend Engineer"
+  },
+  {
+    id: 8,
+    name: "UI Developer"
+  }
+];
+
+export default products;
